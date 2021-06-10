8ce7f4696ad338cb3a9625f774f652232aad43b5
Evandro oliveira <vandoaparecido@hotmail.com>
2021-06-10 00:07:41 -0300

    Criando e estilizando component title e subtitle

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,11 +1,16 @@
 import Head from 'next/head';
 import SafeEnviroment from 'ui/components/feedback/SafeEnviroment/SafeEnviroment';
-
+import PageTitle from 'ui/components/data-display/PageTitle/PageTitle';
 
 export default function Home() {
   return (
     <div>
       <SafeEnviroment />
+      <PageTitle
+       title={'Conheça os profissionais'}
+       subtitle={'Preencha seu email e veja todas as novidades dos profissionais da sua área'}
+       />
+      
     </div>
   )
 }
---
+++ frontend/src/ui/components/data-display/PageTitle/PageTitle.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {PageTitleContainer, PageTitleStyled} from './PageTitle.style'
+
+interface PageTitleProps{
+    title:string;
+    subtitle:string;
+}
+
+const PageTitle: React.FC<PageTitleProps> = (props) =>{
+    return (
+    <PageTitleContainer>
+        <PageTitleStyled>{props.title}</PageTitleStyled>
+        
+        {props.subtitle}
+    </PageTitleContainer>
+    );
+   
+    
+}
+export default PageTitle;

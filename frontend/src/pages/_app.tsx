b1ff75acfba7795479c8d4a6a4fe3b75239b68d0
Evandro oliveira <vandoaparecido@hotmail.com>
2021-06-11 00:41:04 -0300

    Criando footer da aplicação

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -3,6 +3,8 @@ import { ThemeProvider } from "@material-ui/core";
 import theme from "ui/themes/theme";
 import Head from 'next/head';
 import Header from 'ui/components/surfaces/Header/Header';
+import Footer from "ui/components/surfaces/Footer/Footer";
+import { AppContainer } from "ui/styles/pages/_app.style";
 
 function MyApp({ Component, pageProps }) {
   return(
@@ -23,9 +25,13 @@ function MyApp({ Component, pageProps }) {
         />
   </Head>
     <ThemeProvider theme={theme}>
-      <Header />
-      <Component {...pageProps} />
-    </ThemeProvider>;
+      <AppContainer>
+        <Header />
+          <Component {...pageProps} />
+        <Footer />
+      </AppContainer>
+     
+    </ThemeProvider>
   </>
   )
 }

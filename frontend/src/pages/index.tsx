f40c749dd5ea4ea3711ab76ca96cdbe55d6faf4a
Evandro oliveira <vandoaparecido@hotmail.com>
2021-06-12 00:00:58 -0300

    Criando campo input com mascara

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import SafeEnviroment from 'ui/components/feedback/SafeEnviroment/SafeEnviroment';
 import PageTitle from 'ui/components/data-display/PageTitle/PageTitle';
 import UserInformation from 'ui/components/data-display/UserInformation/UserInformation';
+import TextFieldMask from 'ui/components/inputs/TextFieldMask/TextFieldMask'
 
 export default function Home() {
   return (
@@ -10,6 +11,13 @@ export default function Home() {
       <PageTitle
        title={'Conheça os profissionais'}
        subtitle={'Preencha seu email e veja todas as novidades dos profissionais da sua área'}
+       />
+
+       <TextFieldMask
+          mask={"99.999-999"}
+          label={"Digite seu CEP"}
+          fullWidth
+          variant={"outlined"}
        />
       
       <UserInformation

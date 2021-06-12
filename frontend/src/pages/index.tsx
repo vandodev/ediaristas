6bc7ed359f251000307169e8889841e4bfbcc91f
Evandro oliveira <vandoaparecido@hotmail.com>
2021-06-12 00:50:50 -0300

    Formatando área de profissionais

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -4,8 +4,8 @@ import PageTitle from 'ui/components/data-display/PageTitle/PageTitle';
 import UserInformation from 'ui/components/data-display/UserInformation/UserInformation';
 import TextFieldMask from 'ui/components/inputs/TextFieldMask/TextFieldMask'
 
-import {Button, Typography} from "@material-ui/core";
-import {FormElementsContainer} from "ui/styles/pages/index.style";
+import {Button, Typography, Container} from "@material-ui/core";
+import {FormElementsContainer, ProfissionaisPaper, ProfissionaisContainer} from "ui/styles/pages/index.style";
 
 export default function Home() {
   return (
@@ -16,32 +16,77 @@ export default function Home() {
        subtitle={'Preencha seu email e veja todas as novidades dos profissionais da sua área'}
        />
 
-       <FormElementsContainer>
+      <Container>
 
-        <TextFieldMask
-            mask={"99.999-999"}
-            label={"Digite seu CEP"}
-            fullWidth
-            variant={"outlined"}
-        />
+        <FormElementsContainer>
 
-        <Typography color="error">CEP inválido</Typography>
+          <TextFieldMask
+              mask={"99.999-999"}
+              label={"Digite seu CEP"}
+              fullWidth
+              variant={"outlined"}
+          />
 
-        <Button
-          variant="contained"
-          color="secondary"
-          sx={{ width: "220px" }}
-        >Buscar</Button>
+          <Typography color="error">CEP inválido</Typography>
 
-       </FormElementsContainer>
+          <Button
+            variant="contained"
+            color="secondary"
+            sx={{ width: "220px" }}
+          >Buscar</Button>
 
-      
-      <UserInformation
-        name={'Evandro Oliveira'}
-        picture={'https://github.com/vandodev.png'}
-        rating={4}
-        description={'Morro Agudo'}
-      />
+        </FormElementsContainer>
+
+        <ProfissionaisPaper>
+
+          <ProfissionaisContainer>
+
+            <UserInformation
+              name={'Evandro Oliveira'}
+              picture={'https://github.com/vandodev.png'}
+              rating={4}
+              description={'Morro Agudo'}
+            />
+
+            <UserInformation
+              name={'Evandro Oliveira'}
+              picture={'https://github.com/vandodev.png'}
+              rating={4}
+              description={'Morro Agudo'}
+            />
+
+            <UserInformation
+              name={'Evandro Oliveira'}
+              picture={'https://github.com/vandodev.png'}
+              rating={4}
+              description={'Morro Agudo'}
+            />
+
+            <UserInformation
+              name={'Evandro Oliveira'}
+              picture={'https://github.com/vandodev.png'}
+              rating={4}
+              description={'Morro Agudo'}
+            />
+
+            <UserInformation
+              name={'Evandro Oliveira'}
+              picture={'https://github.com/vandodev.png'}
+              rating={4}
+              description={'Morro Agudo'}
+            />
+
+            <UserInformation
+              name={'Evandro Oliveira'}
+              picture={'https://github.com/vandodev.png'}
+              rating={4}
+              description={'Morro Agudo'}
+            />
+
+          </ProfissionaisContainer>
+        </ProfissionaisPaper>
+      </Container>
+
 
     </div>
   )
